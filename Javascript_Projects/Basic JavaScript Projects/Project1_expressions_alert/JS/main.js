db137788b9006c557c0195b08f752471b4558b7d
josephlister <joseph.p.lister@gmail.com>
2021-07-22 19:58:00 -0400

    Created JavaScript_Projects

---
+++ Javascript_Projects/Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+var Movie="Jaws", Captain= "Quint", Chief= "Brody", Scientist= "Hooper", Shark= "Bruce";
+
+var Captain= Captain.fontcolor("red");  //Assigning color to variables
+var Chief= Chief.fontcolor("yellow");
+var Scientist= Scientist.fontcolor("green");
+var Shark= Shark.fontcolor("blue")
+
+document.write(Shark + Captain + Chief + Scientist)  //Concatenating variables
+
+window.alert(Movie) //Using window.alert method
+
+Currentyear= "The year the person types in the box" //This is an expression
+Releaseyear= "1975"
