8c5e8240d4faa21053fe860734e830095ce7f6a4
josephlister <joseph.p.lister@gmail.com>
2021-07-24 19:01:19 -0400

    Updated Project4_dictionaries

---
+++ Javascript_Projects/Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,10 @@
+function My_dictionary() {
+    var Animal = {
+        species = "dog",
+        Nickname = "Chewy",
+        age = 6,
+        color = "brown"
+    };
+    delete Animal.nickname; // Utilizing the delete operator
+document.getElementById("dictionary").innerHTML = Animal.Nickname;
+}
