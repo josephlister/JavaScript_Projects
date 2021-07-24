367c7de507c6e73e163ed0d97f50d76878d64b46
josephlister <joseph.p.lister@gmail.com>
2021-07-24 19:45:39 -0400

    Fixed Project4_dictionaries
    Fixed syntax errors in index.html and main.js

--- Javascript_Projects/Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Javascript_Projects/Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -1,9 +1,9 @@
 function My_dictionary() {
     var Animal = {
-        species = "dog",
-        Nickname = "Chewy",
-        age = 6,
-        color = "brown"
+        species:"dog",
+        Nickname:"Chewy",
+        age:6,
+        color:"brown"
     };
     delete Animal.nickname; // Utilizing the delete operator
 document.getElementById("dictionary").innerHTML = Animal.Nickname;
